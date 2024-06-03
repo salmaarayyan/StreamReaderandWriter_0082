e4dcceb904e431967a59d85a3c8ff72067a82afd
salmaarayyan <salmaarayyan@gmail.com>
2024-06-03 14:56:21 +0700

    menampilkan data perbaris

--- iOFileWithPath.cpp
+++ iOFileWithPath.cpp
@@ -38,5 +38,13 @@ int main() {
 
     cout << endl << ">= Membuka dan membaca file " << endl;
 
-    
+    //jika file ada maka
+    if (infile.is_open()) {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)) {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+
+    }
 }
