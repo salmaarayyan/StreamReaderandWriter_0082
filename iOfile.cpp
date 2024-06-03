e8eb18c19794e19c0968e9957cf36b3089a91d2f
salmaarayyan <salmaarayyan@gmail.com>
2024-06-03 14:30:04 +0700

    membuka file dalam mode membaca

--- iOfile.cpp
+++ iOfile.cpp
@@ -26,6 +26,13 @@ int main() {
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
 
 
 
