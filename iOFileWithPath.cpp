c76d0eabec40d40e5c90b6b84e621a9d7c084f0a
salmaarayyan <salmaarayyan@gmail.com>
2024-06-03 14:49:04 +0700

    menulis dan memasukkan data ke dalam file

--- iOFileWithPath.cpp
+++ iOFileWithPath.cpp
@@ -16,5 +16,15 @@ int main() {
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-    
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
