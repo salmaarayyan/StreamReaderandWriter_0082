29d3ca14e9b160ae1a13594cce04d9a7afa835e4
salmaarayyan <salmaarayyan@gmail.com>
2024-06-03 14:43:47 +0700

    membuka file dalam mode menulis

--- iOFileWithPath.cpp
+++ iOFileWithPath.cpp
@@ -9,4 +9,12 @@ int main() {
 
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
 }
